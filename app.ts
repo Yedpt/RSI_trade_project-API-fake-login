01868e68e19e761902849e361ad0d19a2883f9cd
dario3111 <dario3pacheco@gmail.com>
2024-11-29 11:20:38 +0100

    Api login: sync tables

--- app.ts
+++ app.ts
@@ -6,34 +6,29 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/authRoutes";
 
-
 export const app = express();
 
-app.use (cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 
 app.use("/api/users", authRouter);
 
 const initDB = async (sequelize: Sequelize) => {
-    try {
-        await sequelize.authenticate();
-        console.log("Conexión exitosa a la base de datos.");
+  try {
+    await sequelize.authenticate();
+    console.log("Conexión exitosa a la base de datos.");
 
-        await UserModel.sync({ force: false });
-        console.log("Tabla de usuarios sincronizada.");
-
-        
-    } catch (error) {
-        console.error("Error al conectar la base de datos:", error);
-    }
+    await UserModel.sync({ force: false });
+    console.log("Tabla de usuarios sincronizada.");
+  } catch (error) {
+    console.error("Error al conectar la base de datos:", error);
+  }
 };
 
 initDB(connectionDB);
 
-
 // Iniciar servidor
 export const server = app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
